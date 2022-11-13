2908938e1e078f1607dd04cd071f8960fa9996f5
uwtrigeo <115599976+uwtrigeo@users.noreply.github.com>
2022-11-13 14:19:44 -0800

    lab6

--- scripts.js
+++ scripts.js
@@ -62,55 +62,55 @@ map.on('load', () => {
 map.on('idle', () => {
     // If these two layers were not added to the map, abort
     if (!map.getLayer('trails-layer') || !map.getLayer('boundary-layer')) {
-    return;
+        return;
     }
      
     // Enumerate ids of the layers.
     const toggleableLayerIds = ['trails-layer', 'boundary-layer'];
      
     // Set up the corresponding toggle button for each layer.
-    for (const id of toggleableLayerIds) {
-    // Skip layers that already have a button set up.
-    if (document.getElementById(id)) {
-    continue;
+   for (const id of toggleableLayerIds) {
+        // Skip layers that already have a button set up.
+        if (document.getElementById(id)) {
+        continue;
     }
      
-    // Create a link.
-    const link = document.createElement('a');
-    link.id = id;
-    link.href = '#';
-    link.textContent = id;
-    link.className = 'active';
-     
-    // Show or hide layer when the toggle is clicked.
-    link.onclick = function (e) {
-        const clickedLayer = this.textContent;
-        e.preventDefault();
-        e.stopPropagation();
-     
-        const visibility = map.getLayoutProperty(
-            clickedLayer,
-            'visibility'
-        );
-     
-    // Toggle layer visibility by changing the layout object's visibility property.
-    if (visibility === 'visible') {
-        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-        this.className = '';
-        } else {
-        this.className = 'active';
-        map.setLayoutProperty(
-        clickedLayer,
-        'visibility',
-        'visible'
-        );
-        }
-    };
-     
-    const layers = document.getElementById('menu');
-    layers.appendChild(link);
+        // Create a link.
+        const link = document.createElement('a');
+        link.id = id;
+        link.href = '#';
+        link.textContent = id;
+        link.className = 'active';
+        
+        // Show or hide layer when the toggle is clicked.
+        link.onclick = function (e) {
+            const clickedLayer = this.textContent;
+            e.preventDefault();
+            e.stopPropagation();
+        
+            const visibility = map.getLayoutProperty(
+                clickedLayer,
+                'visibility'
+            );
+        
+        // Toggle layer visibility by changing the layout object's visibility property.
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+            } else {
+                this.className = 'active';
+                map.setLayoutProperty(
+                    clickedLayer,
+                    'visibility',
+                    'visible'
+                );
+            }
+        };
+        
+        const layers = document.getElementById('menu');
+        layers.appendChild(link);
     }
-    });
+});
 
 
 // rendering terrain in 3D
